81ca3e604ab8af9e0fceaf5515a94125bfd8bae0
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-03-13 14:49:46 +0100

    add MILESTONE 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,36 @@
 console.log('yes, sir');
-
 const { createApp } = Vue
+
+const toDoList = [
+
+    {
+        text: 'have breakfast',
+        done: true
+    },
+    {
+        text: 'let the dog out',
+        done: true
+    },
+    {
+        text: 'code the entire javascript',
+        done: false
+    },
+    {
+        text: 'have a snack',
+        done: false
+    },
+    {
+        text: 'play fc24',
+        done: false
+    },
+]
 
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+        toDoList: toDoList,
+        text: toDoList.text,
+        done: true
     }
   }
 }).mount('#app')
